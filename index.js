f429e48335aaaf8e754198114dc0d3362094f92a
tamaneginorio <suzukin343@gmail.com>
2021-06-24 12:28:47 +0900

    two line chart animation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const grid = new contrib.grid({
   cols:2,
   screen: screen
 })
-const lineConfig = { 
+const lineConfig = (title) => ({ 
   style: { 
     line: "yellow",
     text: "green",
@@ -14,16 +14,40 @@ const lineConfig = {
   },
   xLabelPadding: 3,
   xPadding: 5,
-  label: 'Title',
-}
-const line = grid.set(0,0,1,1, contrib.line, lineConfig)
-const line2 = grid.set(0,1,1,1, contrib.line, lineConfig)
-const lineData = {
-  x: ['t1', 't2', 't3', 't4'],
+  label: title,
+})
+const line = grid.set(0,0,1,1, contrib.line, lineConfig('line1'))
+const line2 = grid.set(0,1,1,1, contrib.line, lineConfig('line2'))
+let lineData = {
+  x: [1, 2, 3, 4],
   y: [5, 1, 7, 5]
 }
+let lineData2 = {
+  x: [1, 2, 3, 4],
+  y: [5, 1, 7, 10]
+}
 line.setData([lineData])
-line2.setData([lineData])
+line2.setData([lineData2])
+
+setInterval(() => {
+  lineData = _updateLineData(lineData)
+  lineData2 = _updateLineData(lineData2)
+  line.setData(lineData)
+  line2.setData(lineData2)
+  screen.render()
+}, 500)
+
+const _updateLineData = (lineData) => {
+  const maxLength = 100 
+  const x = lineData.x[lineData.x.length - 1]
+  lineData.x.push(x + 1)
+  lineData.y.push(Math.random()*10+1)
+  if(lineData.x.length > maxLength) {
+    lineData.x.shift()
+    lineData.y.shift()
+  }
+  return lineData
+}
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
