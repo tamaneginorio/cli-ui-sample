ccaa326cf0465490cc7b327cc3535b41d9e1851b
tamaneginorio <suzukin343@gmail.com>
2021-06-24 14:26:33 +0900

    added markdown

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const blessed = require('blessed')
 const contrib = require('blessed-contrib')
 const screen = blessed.screen()
 const grid = new contrib.grid({
-  rows: 2,
+  rows: 6,
   cols:2,
   screen: screen
 })
@@ -17,8 +17,8 @@ const lineConfig = (title) => ({
   xPadding: 5,
   label: title,
 })
-const line = grid.set(0,0,1,1, contrib.line, lineConfig('line1'))
-const line2 = grid.set(0,1,1,1, contrib.line, lineConfig('line2'))
+const line = grid.set(1,0,3,1, contrib.line, lineConfig('line1'))
+const line2 = grid.set(1,1,3,1, contrib.line, lineConfig('line2'))
 let lineData = {
   x: [1, 2, 3, 4],
   y: [5, 1, 7, 5]
@@ -55,13 +55,22 @@ const logConfig = (title) => ({
   selectedFg: "blue",
   label: title 
 })
-const log = grid.set(1,0,1,2, contrib.log, logConfig('LOG'))
+const log = grid.set(4,0,2,2, contrib.log, logConfig('LOG'))
 
 setInterval(() => {
   const now = moment().format('YYYY/MM/DD HH:mm:ss')
   const msg = `now is ${now}`
   log.log(msg)
 }, 1000)
+
+const markdown = grid.set(0,0,1,2, contrib.markdown, null)
+setInterval(() => {
+  const price = parseInt(Math.random() * 20000 + 3500000).toLocaleString()
+  const rate = (Math.random() * 1 + 3).toFixed(2)
+  const spread = parseInt(Math.random() * 2000 + 50).toLocaleString()
+  const msg = `PRICE: \`${price}\` RATE: \`${rate}\` SPREAD: \`${spread}\``
+  markdown.setMarkdown(msg)
+})
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
